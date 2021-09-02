d19d4d863e453e3f25da4e72def4f60972704566
Isaac Juarez <ijuarez@fujifilm.com.mx>
2021-09-02 09:38:14 -0500

    main

---
+++ ES6/let-vat.js
@@ -0,0 +1,5 @@
+"use strict";
+(function () {
+    console.log('let');
+})();
+//# sourceMappingURL=let-vat.js.map
---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+(function () {
+    console.log('let');
+})();
+//# sourceMappingURL=main.js.map

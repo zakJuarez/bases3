49a21cc6da3428b285efc18adad89113376565ae
Isaac Juarez <ijuarez@fujifilm.com.mx>
2021-09-02 16:22:52 -0500

    variables let

--- ES6/let-vat.ts
+++ ES6/let-vat.ts
@@ -1,3 +1,15 @@
 (() => {
     console.log('let');
+    const nombre = 'Isaac';
+    console.log(nombre);
+    const getName = () => {
+        console.log('viejo getName');
+    }
+
+    // getName = () => {
+    //     console.log('nuevo getName');
+    // }
+
+    // getName();
+
 })()
--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 "use strict";
-(function () {
+(() => {
     console.log('let');
+    const nombre = 'Isaac';
+    console.log(nombre);
+    const getName = () => {
+        console.log('viejo getName');
+    };
 })();
 //# sourceMappingURL=main.js.map

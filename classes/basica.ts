b5f85689712032f6f00d5a1ba2a13ba36507bb9b
Isaac Juarez <ijuarez@fujifilm.com.mx>
2021-09-03 10:38:50 -0500

    clase basica

---
+++ classes/basica.ts
@@ -0,0 +1,18 @@
+(()=>{
+    class Avenger {
+        private name:string;
+        public team:string;
+        public realName: string;
+        static avgAge: number = 35;
+
+        constructor(name:string, team: string, realName?: string){
+            this.name =name;
+            this.team = team;
+            this.realName = realName;
+        }
+    }
+
+    const antman :Avenger = new Avenger('Antman','Captain');
+
+    console.log(antman);
+})()
--- main.js
+++ main.js
@@ -1,42 +1,14 @@
 "use strict";
 (() => {
-    class avenger {
-        constructor(name = 'no_name', power = 0) {
+    class Avenger {
+        constructor(name, team, realName) {
             this.name = name;
-            this.power = power;
+            this.team = team;
+            this.realName = realName;
         }
     }
-    class flyingAvenger extends avenger {
-        constructor(name, power) {
-            super(name, power);
-            this.flying = true;
-        }
-    }
-    const Hulk = new avenger('Hulk', 9001);
-    const Falcon = new flyingAvenger('Falcon', 60);
-    console.log(Hulk);
-    console.log(Falcon);
-})();
-(() => {
-    const avengers = {
-        nick: 'Samuel L Jackson',
-        ironman: 'Robert Dawni Jr',
-        vision: 'Paul Betanny',
-        activo: true,
-        poder: 5000
-    };
-    const avengerArr = ['Capitan America', true, 150.5];
-    const [capitan, ironman,] = avengerArr;
-})();
-(() => {
-    const ironman = { name: 'Ironman', weapon: 'Armorsuir' };
-    const captainAmerica = { name: 'Capital America', weapon: 'Shield' };
-    const thor = { name: 'Thor', weapon: 'Mholnir' };
-    const avengers = [ironman, captainAmerica, thor];
-})();
-(() => {
-    const getName = () => {
-        console.log('viejo getName');
-    };
+    Avenger.avgAge = 35;
+    const antman = new Avenger('Antman', 'Captain');
+    console.log(antman);
 })();
 //# sourceMappingURL=main.js.map

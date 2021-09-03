332808085ada5e82d188d0a5255792d69ef067f6
Isaac Juarez <ijuarez@fujifilm.com.mx>
2021-09-03 16:38:58 -0500

    clases abstractas

---
+++ classes/abstract.ts
@@ -0,0 +1,24 @@
+(() => {
+    abstract class Mutante{
+        constructor(public name: string, public realName: string){
+
+        }
+    }
+
+    class Xmen extends Mutante {
+        salvarMundo(){
+            return `Mundo a salvo!...`;
+        }
+    }
+
+    class Villian extends Mutante{
+        conquistarMundo(){
+            return `Mundo conquistado!!`;
+        }
+    }
+
+    const wolverine = new Xmen('Wolverine', 'Logan');
+    const magneto = new Villian('Magneto', 'Magnus');
+    // console.log(wolverine.salvarMundo());
+    // console.log(magneto.conquistarMundo());
+})()
